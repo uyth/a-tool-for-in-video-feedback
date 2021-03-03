fd724aa0d171f911982d825fe2b1416f37e479e4
uyth <uyth95@gmail.com>
2021-03-03 16:10:47 +0100

    Log events in sessions

--- server/index.js
+++ server/index.js
@@ -63,6 +63,7 @@ async function receiveMessage(ID, payload, socket) {
 async function processEvent(socket, data) {
     console.log("event: " + data.event.eventType);
     console.log("event: " + data.event.videoSnapshot.currentTime);
+    await wsController.updateSession(data.session, data.event);
     if (data.event.eventType == "PAUSE") sendFeedback(socket, data);
 }
 
