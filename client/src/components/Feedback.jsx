dba5bf6f2a284b3ca5a9748372c2c587e16f47af
uyth <uyth95@gmail.com>
2021-04-01 17:22:29 +0200

    Remove dead code

--- client/src/components/Feedback.jsx
+++ client/src/components/Feedback.jsx
@@ -7,32 +7,6 @@ import HelpIcon from '@material-ui/icons/Help';
 
 const reactStringReplace = require('react-string-replace');
 
-function FeedbackCard({props}) {
-    return (
-        <Card>
-            <Card.Body>
-                <Card.Title>{props.title}</Card.Title>
-                <Card.Text>Keywords: {props.keywords.join(", ")}</Card.Text>
-                <div>
-                    <Button variant="primary" href={props.link}>Go to source</Button>
-                    <OverlayTrigger trigger="focus" placement="top" overlay={
-                        <Popover>
-                            <Popover.Title as="h3">Why this feedback?</Popover.Title>
-                            <Popover.Content>
-                                <p>Based on the timerange: {props.timerange.join(" - ")}</p>
-                                <p>Based on keywords: {props.keywords.join(", ")}</p>
-                                <p>Triggered @{props.timestamp}</p>
-                            </Popover.Content>
-                        </Popover>                    
-                    }>
-                        <Button variant="secondary">Why this feedback?</Button>
-                    </OverlayTrigger>
-                </div>
-            </Card.Body>
-        </Card>
-    )
-}
-
 function decodeHtml(html) {
     var txt = document.createElement("textarea");
     txt.innerHTML = html;
